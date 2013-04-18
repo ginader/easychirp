020dbb786eeb07e333a265af67a28e71daa35660
weboverhauls <weboverhauls@gmail.com>
2013-04-17 22:40:31 -0700

    Adding headings. Modifying heading style.

--- site/application/views/about.php
+++ site/application/views/about.php
@@ -1,4 +1,47 @@
 <h1>About</h1>
-<p>
- Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.	
-</p>
+
+<h2>The Name</h2>
+<p>TBD</p>
+
+<h2>Our Twitter Account</h2>
+<p>TBD</p>
+
+<h2>Donate</h2>
+<p>TBD</p>
+
+<h2>Feedback &amp; Email</h2>
+<p>TBD</p>
+
+<h2>Promote Easy Chirp!</h2>
+<p>TBD</p>
+
+<h2>Technical</h2>
+<p>TBD</p>
+
+<h2>Disclaimer</h2>
+<p>TBD</p>
+
+<h2>Awards!</h2>
+<p>TBD</p>
+
+<h3>Recipient of the AFB 2011 Access Award</h3>
+<p>TBD</p>
+
+<h3>Recipient of the 2009 Access IT @web2.0 Award</h3>
+<p>TBD</p>
+
+<h3>Nominated in the 2010 Best of the Web Awards</h3>
+<p>TBD</p>
+
+<h3>Nominated in the Blind Bargains: 2009 &amp; 2010 Access Awards</h3>
+<p>TBD</p>
+
+<h3>RNIB Featured Website, December 2011</h3>
+<p>TBD</p>
+
+<h2>Authors</h2>
+<p>TBD</p>
+
+<h2>More Info</h2>
+<p>TBD</p>
+
--- site/application/views/home.php
+++ site/application/views/home.php
@@ -1,5 +1,41 @@
-<div>
-	<p>
-	hello world! this is the homepage for EasyChirp!
-	</p>
-</div>
+<h1>Easy Chirp 2</h1>
+
+<p>Easy Chirp is a web-accessible alternative to the Twitter.com website. It is designed to be easier to use and is optimized for disabled users. It also works with keyboard-only, older browsers like IE6, lowband internet connection, and without JavaScript. Learn more about web accessibility.
+Refer to the development tasks further down this page for items currently in progress. Learn more about Easy Chirp on the About and Features pages. You may also read an extensive list of user feedback and blogs/articles about this application.</p>
+
+<h2>Sign In</h2>
+<p>TBD</p>
+
+<h2>Donate to Easy Chirp</h2>
+<p>TBD</p>
+
+<h2>Latest Tweets from Easy Chirp</h2>
+<p>TBD</p>
+
+<h2>Share</h2>
+<p>TBD</p>
+
+<h2>Article & Blog Mentions</h2>
+<p>TBD</p>
+
+<h2>What People Are Tweeting</h2>
+<p>TBD</p>
+
+<h2>Notable Features</h2>
+<p>TBD</p>
+
+<h2>Awards!</h2>
+<p>TBD</p>
+
+<h2>Development Tasks</h2>
+<p>TBD</p>
+
+<h3>Known Issues</h3>
+<p>TBD</p>
+
+<h3>Current Tasks</h3>
+<p>TBD</p>
+
+<h3>Wish List</h3>
+<p>TBD</p>
+
