09fd62b12380093cba0068b49ffcc39e0d5d45b5
Lembree <weboverhauls@gmail.com>
2013-04-28 13:15:53 -0700

    content changes

--- site/application/views/features.php
+++ site/application/views/features.php
@@ -3,13 +3,49 @@
 <div class="box1 rounded">
 	<h2>Accessibility Features</h2>
 	<ul>
-		<li>TBD</li>
+		<li>All links and functionality is keyboard accessible.
+		<li>Simple, consistent layout and navigation is provided.
+		<li>Headings and page titles are implemented carefully for optimal accessibility.
+		<li>Ajax actions are concluded with an alert (JS or ARIA) that notifies the user of the result of the action.
+		<li>Large default text size and high high color contrast.
+		<li>Text may be resized many levels without breaking layout.
+		<li>Access keys (native keyboard shortcuts) to main menu items.
+		<li>Forms and data tables are marked up for optimal accessibility.
+		<li>Code is semantic, light, and adheres to best practices for Web Standards.
+		<li>ARIA is implemented throughout including document landmark roles, alert roles, and live region for the dynamic character counter.
+		<li>A "skip to main content" link is provided for keyboard users (and a "skip to sign in" link on the home page).
+		<li>Works with or without JavaScript.
+		<li>Works with all types of assistive technology including screen readers, screen enlargers, braille-output, text-only browsers, and pretty much any web-enabled device.
 	</ul>
 </div>
 
 <div class="box1 rounded">
 	<h2>Main Features</h2>
 	<ul>
-		<li>TBD</li>
+		<li>When writing a tweet, a character counter and a Shorten URL feature (choose bit.ly or weba.im) are available.
+		<li>In addition to the basic retweet, quote tweet, and reply, user can reply to all and email tweet.
+		<li>View, subscribe, and create Twitter Lists.
+		<li>Search tweets and users (and search tips).
+		<li>Add, delete, and run saved searches.
+		<li>Edit basic profile settings including updating avatar.
+		<li>Easily jump to user's profile and timeline.
+		<li>Inline threading of tweets; displays reply "conversation".
+		<li>Tweets contain linked hash tags and usernames.
+		<li>Easy to review a tweet when replying and quoting.
+		<li>View details of who you're following and your followers.
+		<li>Blocking is supported.
+		<li>Number of retweets are displayed on each tweet (if 1 or more).
+		<li>View Retweets by you, of you, and to you.
+		<li>Quickly find out if a user is following you from the user's profile page.
+		<li>View current Twitter Trends.
+		<li>A link to Twitter is provided for 'View Single Tweet' page and for owned and subscribed lists.
+		<li>Designed for all screen resolutions, great for desktop, tablet and mobile.
+		<li>Works on all major browsers: IE7, IE8, IE9, Firefox, Safari, Opera, and Chrome.
 	</ul>
 </div>
+
+	
+
+
+
+
