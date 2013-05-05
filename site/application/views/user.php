ce0f98c3cde189c6a2baf0e5adae7b722b2f0b04
weboverhauls <weboverhauls@gmail.com>
2013-05-04 17:18:36 -0700

    Modify/add content for Report Spammer and Block User.

--- site/application/views/user.php
+++ site/application/views/user.php
@@ -9,6 +9,8 @@
 	<h3>{name}</h3>
 	<p>Following (<a href="#">Unfollow</a>) | Not Following (<a href="#">Follow</a>)</p>
 	<p><a href="#">Is {username} following me?</a> YES | NO</p>
+	<p><span aria-hidden="true" class="icon-alert"></span> <a href="#">Report Spammer</a></p>
+	<p><span aria-hidden="true" class="icon-blocked"></span> <a href="#">Block User</a></p>
 	<p><span aria-hidden="true" class="icon-checkmark"></span> Verified User</p>
 </div>
 
