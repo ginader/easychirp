b8dac669f85a5b7178d56e03a6a42c95dd874e33
Dennis Lembree <dlembree@paypal.com>
2013-05-14 15:37:50 -0700

    Removing class on UL (remnant)

--- site/application/views/retweets.php
+++ site/application/views/retweets.php
@@ -2,7 +2,7 @@
 
 <div class="box1 rounded">
 	<h2>Select a page:</h2>
-	<ul class="submenu">
+	<ul>
 		<li><a href="/app/retweets_by_me">Retweets By Me</a></li>
 		<li><a href="/app/retweets_of_me">Retweets Of Me</a></li>
 		<li><a href="/app/retweets_to_me">Retweets To Me</a></li>
