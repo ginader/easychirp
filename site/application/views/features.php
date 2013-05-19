f601778d83699da23ba947e7cb6eeb22adef4ed9
weboverhauls <weboverhauls@gmail.com>
2013-05-18 20:40:28 -0700

    xliff-ing (Features page)

--- site/application/views/features.php
+++ site/application/views/features.php
@@ -1,46 +1,46 @@
 <h1 class="rounded">Features</h1>	
 
 <div class="box1 rounded">
-	<h2>Accessibility Features</h2>
+	<h2><?php echo $xliff_reader->get('features-a11y-h2'); ?></h2>
 	<ul>
-		<li>All links and functionality is keyboard accessible.
-		<li>Simple, consistent layout and navigation is provided.
-		<li>Headings and page titles are implemented carefully for optimal accessibility.
-		<li>Ajax actions are concluded with an alert (JS or ARIA) that notifies the user of the result of the action.
-		<li>Large default text size and high high color contrast.
-		<li>Text may be resized many levels without breaking layout.
-		<li>Access keys (native keyboard shortcuts) to main menu items.
-		<li>Forms and data tables are marked up for optimal accessibility.
-		<li>Code is semantic, light, and adheres to best practices for Web Standards.
-		<li>ARIA is implemented throughout including document landmark roles, alert roles, and live region for the dynamic character counter.
-		<li>A "skip to main content" link is provided for keyboard users (and a "skip to sign in" link on the home page).
-		<li>Works with or without JavaScript.
-		<li>Works with all types of assistive technology including screen readers, screen enlargers, braille-output, text-only browsers, and pretty much any web-enabled device.
+		<li><?php echo $xliff_reader->get('features-a11y-kybd'); ?>
+		<li><?php echo $xliff_reader->get('features-a11y-layout'); ?>
+		<li><?php echo $xliff_reader->get('features-a11y-headings'); ?>
+		<li><?php echo $xliff_reader->get('features-a11y-ajax'); ?>
+		<li><?php echo $xliff_reader->get('features-a11y-contrast'); ?>
+		<li><?php echo $xliff_reader->get('features-a11y-resize'); ?>
+		<li><?php echo $xliff_reader->get('features-a11y-keys'); ?>
+		<li><?php echo $xliff_reader->get('features-a11y-forms'); ?>
+		<li><?php echo $xliff_reader->get('features-a11y-semantic'); ?>
+		<li><?php echo $xliff_reader->get('features-a11y-aria'); ?>
+		<li><?php echo $xliff_reader->get('features-a11y-skip'); ?>
+		<li><?php echo $xliff_reader->get('features-a11y-js'); ?>
+		<li><?php echo $xliff_reader->get('features-a11y-at'); ?>
 	</ul>
 </div>
 
 <div class="box1 rounded">
-	<h2>Main Features</h2>
+	<h2><?php echo $xliff_reader->get('features-main-h2'); ?></h2>
 	<ul>
-		<li>When writing a tweet, a character counter and a Shorten URL feature (choose bit.ly or weba.im) are available.
-		<li>In addition to the basic retweet, quote tweet, and reply, user can reply to all and email tweet.
-		<li>View, subscribe, and create Twitter Lists.
-		<li>Search tweets and users (and search tips).
-		<li>Add, delete, and run saved searches.
-		<li>Edit basic profile settings including updating avatar.
-		<li>Easily jump to user's profile and timeline.
-		<li>Inline threading of tweets; displays reply "conversation".
-		<li>Tweets contain linked hash tags and usernames.
-		<li>Easy to review a tweet when replying and quoting.
-		<li>View details of who you're following and your followers.
-		<li>Blocking is supported.
-		<li>Number of retweets are displayed on each tweet (if 1 or more).
-		<li>View Retweets by you, of you, and to you.
-		<li>Quickly find out if a user is following you from the user's profile page.
-		<li>View current Twitter Trends.
-		<li>A link to Twitter is provided for 'View Single Tweet' page and for owned and subscribed lists.
-		<li>Designed for all screen resolutions, great for desktop, tablet and mobile.
-		<li>Works on all major browsers: IE7, IE8, IE9, Firefox, Safari, Opera, and Chrome.
+		<li><?php echo $xliff_reader->get('features-main-shortener'); ?>
+		<li><?php echo $xliff_reader->get('features-main-replytoall'); ?>
+		<li><?php echo $xliff_reader->get('features-main-lists'); ?>
+		<li><?php echo $xliff_reader->get('features-main-search'); ?>
+		<li><?php echo $xliff_reader->get('features-main-saved-search'); ?>
+		<li><?php echo $xliff_reader->get('features-main-edit-profile'); ?>
+		<li><?php echo $xliff_reader->get('features-main-jump'); ?>
+		<li><?php echo $xliff_reader->get('features-main-thread'); ?>
+		<li><?php echo $xliff_reader->get('features-main-linked'); ?>
+		<li><?php echo $xliff_reader->get('features-main-review'); ?>
+		<li><?php echo $xliff_reader->get('features-main-foll'); ?>
+		<li><?php echo $xliff_reader->get('features-main-block'); ?>
+		<li><?php echo $xliff_reader->get('features-main-retweets-num'); ?>
+		<li><?php echo $xliff_reader->get('features-main-retweets-types'); ?>
+		<li><?php echo $xliff_reader->get('features-main-isfollow'); ?>
+		<li><?php echo $xliff_reader->get('features-main-trends'); ?>
+		<li><?php echo $xliff_reader->get('features-main-twitter-links'); ?>
+		<li><?php echo $xliff_reader->get('features-main-screens'); ?>
+		<li><?php echo $xliff_reader->get('features-main-browsers'); ?>
 	</ul>
 </div>
 
