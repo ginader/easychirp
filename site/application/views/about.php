e34ee784467ca01a1c7607fc18398768d1d42091
weboverhauls <weboverhauls@gmail.com>
2013-05-20 19:51:35 -0700

    xliff-ing About (partial)

--- site/application/views/about.php
+++ site/application/views/about.php
@@ -1,34 +1,34 @@
-<h1 class="rounded">About</h1>
+<h1 class="rounded"><?php echo $xliff_reader->get('about-h1'); ?></h1>
 
 <div class="box1 rounded">
-	<h2>The Name</h2>
-	<p>A "2" was added to the Easy Chirp name in mid-2013 after a full relaunch (and rewrite). The relaunch was due to major updates needed including a change in the Twitter API. A successful <a href="http://www.kickstarter.com/projects/dlembree/easy-chirp-2-contribute-to-an-inclusive-twittersph">Kickstarter campaign</a> was run in order to help funding for the relaunch.</p>
-	<p>Originally, the app was named &quot;Accessible Twitter&quot;. Many of the articles and awards cited on this website use this name. Learn more <a href="http://www.weboverhauls.com/ce/pressreleases/pr_2011_06_01.htm">about the name change</a>.</p>
+	<h2><?php echo $xliff_reader->get('about-h2-name'); ?></h2>
+	<p><?php echo $xliff_reader->get('about-name-p1'); ?></p>
+	<p><?php echo $xliff_reader->get('about-name-p2'); ?></p>
 </div>
 
 <div class="box1 rounded">
-	<h2>Donate</h2>
-	<p>Help keep Twitter fully web-accessible by donating to Easy Chirp! All donations will go toward site development, maintenance, and hosting fees.</p>
+	<h2><?php echo $xliff_reader->get('about-h2-donate'); ?></h2>
+	<p><?php echo $xliff_reader->get('about-donate-p1'); ?></p>
 	<form action="https://www.paypal.com/cgi-bin/webscr" method="post">
 		<input type="hidden" name="cmd" value="_s-xclick" />
 		<input type="hidden" name="hosted_button_id" value="2JSYK7TQNL5GA" />
-		<input type="image" src="https://www.paypalobjects.com/WEBSCR-640-20110306-1/en_US/i/btn/btn_donateCC_LG.gif" name="submit" alt="Donate with PayPal; credit cards accepted." />
+		<input type="image" src="https://www.paypalobjects.com/WEBSCR-640-20110306-1/en_US/i/btn/btn_donateCC_LG.gif" name="submit" alt="<?php echo $xliff_reader->get('about-donate-alt'); ?>" />
 		<img alt="" src="https://www.paypalobjects.com/WEBSCR-640-20110306-1/en_US/i/scr/pixel.gif" width="1" height="1" />
 	</form>
 </div>
 
 <div class="box1 rounded">
-	<h2><span aria-hidden="true" class="icon-twitter2"></span> Our Twitter Account</h2>
+	<h2><span aria-hidden="true" class="icon-twitter2"></span> <?php echo $xliff_reader->get('about-h2-account'); ?></h2>
 	<p>Follow Easy Chirp 2 on Twitter: <a href="http://twitter.com/EasyChirp">@EasyChirp</a>.</p>
 </div>
 
 <div class="box1 rounded">
-	<h2><span aria-hidden="true" class="icon-mail"></span> Feedback &amp; Email</h2>
+	<h2><span aria-hidden="true" class="icon-mail"></span> <?php echo $xliff_reader->get('about-h2-feedback'); ?></h2>
 	<p>Feedback and suggestions are always welcome. In addition to Twitter, you may contact Easy Chirp by emailing info AT easy chirp dot com.</p>
 </div>
 
 <div class="box1 rounded">
-	<h2>Promote Easy Chirp!</h2>
+	<h2><?php echo $xliff_reader->get('about-h2-promote'); ?></h2>
 	<p><img class="fr" style="margin:0 1em;" src="/images/brand/easy_chirp_icon1.png" alt="Easy Chirp icon" width="48" height="48" /> Show your support for web accessibility by promoting Easy Chirp! Or just because you like it (smiley). Here are some ideas on how:</p>
 	<ul>
 		<li>Use the Easy Chirp application.</li>
@@ -40,18 +40,18 @@
 </div>
 
 <div class="box1 rounded">
-	<h2>Technical</h2>
+	<h2><?php echo $xliff_reader->get('about-h2-technical'); ?></h2>
 	<p>Easy Chirp 2 is built with PHP (with Codeigniter MVC framework), HTML5, JavaScript (and jQuery), and the <a rel="nofollow" href="http://dev.twitter.com/">Twitter API</a>.</p>
 </div>
 
 <div class="box1 rounded">
-	<h2>Disclaimer</h2>
+	<h2><?php echo $xliff_reader->get('about-h2-disclaimer'); ?></h2>
 	<p>This site is not affiliated in any way to <a rel="nofollow" href="http://www.twitter.com">Twitter</a>.</p>
 	<p>Your information is safe with us! The information in your profile, tweets, etc., will not used in any way outside of this web application. Use of this web site is voluntary. This site is not responsible for any data which may be changed or lost.</p>
 </div>
 
 <div class="box1 rounded">
-	<h2>Awards!</h2>
+	<h2><?php echo $xliff_reader->get('about-h2-awards'); ?></h2>
 
 	<h3>Recipient of the AFB 2011 Access Award</h3>
 	<p>Accessible Twitter was honored with  the <a href="http://www.afb.org/Section.asp?DocumentID=5410">American Foundation for the Blind 2011 Access Award</a>. Other recipients included CBS, Lexmark, and Walt Disney Parks &amp; Resorts.</p>
@@ -70,13 +70,13 @@
 </div>
 
 	<div class="box1 rounded">
-	<h2><span aria-hidden="true" class="icon-users"></span> Authors</h2>
+	<h2><span aria-hidden="true" class="icon-users"></span> <?php echo $xliff_reader->get('about-h2-authors'); ?></h2>
 	<p><a href="http://www.dennislembree.com/">Dennis Lembree</a> is the owner and original developer of Easy Chirp/Accessible Twitter. 
 		 Easy Chirp 2 is developed by Dennis, <a href="http://andrewwoods.net/">Andrew Woods</a>, and <a href="http://ginader.com/">Dirk Ginader</a>.</p>
 </div>
 
 <div class="box1 rounded">
-	<h2>More Info</h2>
+	<h2><?php echo $xliff_reader->get('about-h2-more'); ?></h2>
 	<p>For more information, visit the <a href="/features">Features</a> and <a href="/articles">Articles &amp; Feedback</a> pages.</p>
 </div>
 
