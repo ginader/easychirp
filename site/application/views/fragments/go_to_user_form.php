c74bb94ab3a21b4667ddbf9b194856f405f74092
weboverhauls <weboverhauls@gmail.com>
2013-06-09 11:18:26 -0700

    xliff-ing Go To User fragment

--- site/application/views/fragments/go_to_user_form.php
+++ site/application/views/fragments/go_to_user_form.php
@@ -1,9 +1,9 @@
 <form action="actions/goUser.php" method="post" id="frmGoUser">
-	<h3><label for="goUser">Go to User</label></h3>
-	<input placeholder="@UserName" type="text" size="15" name="uid" id="goUser" /> 
+	<h3><label for="goUser"><?php echo $xliff_reader->get('gotouser-h1'); ?></label></h3>
+	<input placeholder="<?php echo $xliff_reader->get('gotouser-username'); ?>" type="text" size="20" name="uid" id="goUser" /> 
 	<input type="radio" name="goUserAction" id="goUserTypeTimeline" value="timeline" checked="checked" />
-	<label for="goUserTypeTimeline">Timeline</label>
+	<label for="goUserTypeTimeline"><?php echo $xliff_reader->get('gotouser-timeline'); ?></label>
 	<input type="radio" name="goUserAction" id="goUserTypeProfile" value="profile" /> 
-	<label for="goUserTypeProfile">Profile</label>
-	<button type="submit">Go</button>
+	<label for="goUserTypeProfile"><?php echo $xliff_reader->get('gotouser-profile'); ?></label>
+	<button type="submit"><?php echo $xliff_reader->get('gotouser-go'); ?></button>
 </form>
