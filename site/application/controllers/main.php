ad5afa7acd1382a0eb7db96aac697e5b1a3ff03b
Andrew Woods <atwoods1@gmail.com>
2013-06-13 15:10:07 -0700

    Updated User Profile with Twitter Data
    Populated the current users profile with Twitter API data.
    Added 'sign in' to main nav - otherwise people cant sign into twitter if not on homepage.

--- site/application/controllers/main.php
+++ site/application/controllers/main.php
@@ -239,6 +239,20 @@ class Main extends EC_Controller {
 		$data = array();
 		$data['xliff_reader'] = $this->xliff_reader; 	
 
+		$params = array();
+		$params[] = $this->config->item('tw_consumer_key');
+		$params[] = $this->config->item('tw_consumer_secret');
+		$params[] = $this->session->userdata('user_oauth_token');
+		$params[] = $this->session->userdata('user_oauth_token_secret');
+
+		$this->load->library('twitter_lib');
+		$this->twitter_lib->connect($params);
+
+		$request_param = array();	
+		$request_param['screen_name'] =  $this->session->userdata('screen_name');
+
+		$data['profile'] = $this->twitter_lib->get('users/show', $request_param );
+
 		$this->layout->set_title('My Profile');
 		$this->layout->set_description('Details on my Twitter profile.');
 		$this->layout->view('profile', $data);
@@ -343,7 +357,6 @@ class Main extends EC_Controller {
 		$params[] = $this->config->item('tw_consumer_secret');
 		$params[] = $this->config->item('tw_access_key');
 		$params[] = $this->config->item('tw_access_secret');
-		// $params[] = base_url() . $this->config->item('tw_callback_url');
 		
 		$this->load->library('twitter_lib');
 		$this->twitter_lib->connect($params);
