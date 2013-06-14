f32f0d12cd88478fe40d6e2abe370e5e52abdf18
weboverhauls <weboverhauls@gmail.com>
2013-06-13 21:35:24 -0700

    creating Tweet include file (dummy data)

---
+++ site/application/views/fragments/tweet.php
@@ -0,0 +1,27 @@
+<div class="tweet rounded clearfix">
+	<div class="tweetAvatar" style="background-image:url(images/avatar_todd.png);"></div>
+	<h2 class="hide">Username 3</h2>
+	<q>The end of the tweet the tweetthe tweet the tweetthe tweet the tweetthe tweet the tweeweetthe tweet the tweetthe tweet the et the tweetthe tweet the tweet</q>
+	<p>from <a href="#" title="fullname; followers; following">username</a> | <a href="#">date</a> | retweet/responding | via <a href="#">app</a></p>
+	<div class="btnOptions">
+		<h3><a href="#tweetOptions_3" class="btnOptionsTweet" title="tweet options" data-icon="&#x29;"><span class="hide">tweet options</span></a></h3>
+		<ul id="tweetOptions_3">
+			<li><a href="#" data-icon="&#x2a;" title="favorite"><span class="hide">favorite</span></a></li>
+			<li><a href="#" data-icon="&#x41;" title="reply"><span class="hide">reply</span></a></li>
+			<li><a href="#" data-icon="&#x3b;" title="reply to all"><span class="hide">reply to all</span></a></li>
+			<li><a href="#" data-icon="&#x3f;" title="retweet"><span class="hide">retweet</span></a></li>
+			<li><a href="#" data-icon="&#x30;" title="quote tweet"><span class="hide">quote tweet</span></a></li>
+			<li><a href="#" data-icon="&#x31;" title="email tweet"><span class="hide">email tweet</span></a></li>
+		</ul>
+	</div>
+	<div class="btnOptions">
+		<h3><a href="#userOptions_3" class="btnOptionsUser" title="user options" data-icon="&#x3c;"><span class="hide">user options</span></a></h3>
+		<ul id="userOptions_3">
+			<li><a href="#" data-icon="&#x3e;" title="view this user's timeline"><span class="hide">timeline</span></a></li>
+			<li><a href="#" data-icon="&#x37;" title="direct message this user"><span class="hide">direct message</span></a></li>
+			<li><a href="#" data-icon="&#x38;" title="tweet message"><span class="hide">tweet message</span></a></li>
+			<li><a href="#" data-icon="&#x3d;" title="muter user"><span class="hide">mute user</span></a></li>
+			<li><a href="#" data-icon="&#x33;" title="report spammer" class="spammer"><span class="hide">report spammer</span></a></li>
+		</ul>
+	</div>
+</div>
