1025b2389a275f5868e6d203be8959cd0c367a84
weboverhauls <weboverhauls@gmail.com>
2013-06-29 21:38:18 -0700

    On About page, adding Kickstarter thank-you section.

--- site/application/views/about.php
+++ site/application/views/about.php
@@ -83,9 +83,16 @@
 	<p><?php echo $xliff_reader->get('about-awards-rnib'); ?></p>
 </div>
 
-	<div class="box1 rounded">
-	<h2><span aria-hidden="true" class="icon-users"></span> <?php echo $xliff_reader->get('about-h2-authors'); ?></h2>
+<div class="box1 rounded">
+	<h2 id="kickstarter"><span aria-hidden="true" class="icon-users"></span> <?php echo $xliff_reader->get('about-h2-authors'); ?></h2>
 	<p><?php echo $xliff_reader->get('about-authors-p1'); ?></p>
+</div>
+
+<div class="box1 rounded">
+	<h2><?php echo $xliff_reader->get('about-h2-kick'); ?></h2>
+	<p><?php echo $xliff_reader->get('about-kick-p1'); ?></p>
+	<p><?php echo $xliff_reader->get('about-kick-p2'); ?></p>
+	<p><em><?php echo $xliff_reader->get('about-kick-names'); ?></em></p>
 </div>
 
 <div class="box1 rounded">
