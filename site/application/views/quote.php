cf2995a07c2f8d0a834a02b6289a21915f73f916
Lembree, Dennis <weboverhauls@gmail.com>
2013-07-27 14:38:08 -0700

    xliff-ing H1 text for Quote and Reply pages

--- site/application/views/quote.php
+++ site/application/views/quote.php
@@ -1,4 +1,4 @@
-<h1 class="rounded">Quote A Tweet</h1>
+<h1 class="rounded"><?php echo $xliff_reader->get('quote-h1'); ?></h1>
 
 <?php
 require_once 'fragments/write_tweet.php';
--- site/application/views/reply.php
+++ site/application/views/reply.php
@@ -1,4 +1,4 @@
-<h1 class="rounded">Reply To Tweet</h1>
+<h1 class="rounded"><?php echo $xliff_reader->get('reply-h1'); ?></h1>
 
 <?php
 require_once 'fragments/write_tweet.php';
