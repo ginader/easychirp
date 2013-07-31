7c5cae24ea9647c657ddfa6554fd2889dccdf52b
Lembree, Dennis <weboverhauls@gmail.com>
2013-07-30 19:38:28 -0700

    Adding REL attr to make modal work.

--- site/application/views/tools.php
+++ site/application/views/tools.php
@@ -3,9 +3,9 @@
 <div class="box1 rounded">
 	<h2>Select a page:</h2>
 	<ul>
-		<li><a href="/go_to_user"><?php echo $xliff_reader->get('nav-goto-user'); ?></a></li>
+		<li><a href="/go_to_user" rel="modal"><?php echo $xliff_reader->get('nav-goto-user'); ?></a></li>
 		<li><a href="/search"><?php echo $xliff_reader->get('nav-search'); ?></a></li>
-		<li><a href="/quick_search"><?php echo $xliff_reader->get('nav-quick-search'); ?></a></li>
+		<li><a href="/search_quick" rel="modal"><?php echo $xliff_reader->get('nav-quick-search'); ?></a></li>
 		<li><a href="/lists"><?php echo $xliff_reader->get('nav-lists'); ?></a></li>
 		<li><a href="/trends"><?php echo $xliff_reader->get('nav-trends'); ?></a></li>
 	</ul>
