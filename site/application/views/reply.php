89ebea40dfefee34c3bcbfabe0a8a5d38065e268
Lembree, Dennis <weboverhauls@gmail.com>
2013-08-02 17:15:29 -0700

    -Creating ability to display write tweet content on page load. -Applying to Quote and Reply pages.

--- site/application/views/reply.php
+++ site/application/views/reply.php
@@ -1,6 +1,11 @@
 <h1 class="rounded"><?php echo $xliff_reader->get('reply-h1'); ?></h1>
 
+<div id="enterTweet" class="single rounded">
 <?php
-require_once 'fragments/write_tweet.php';
+require_once 'fragments/write_tweet_form.php';
+?>
+</div>
+
+<?php
 echo $tweets;
 ?>
