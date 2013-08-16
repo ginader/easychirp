5206336e741a43410a398c32ff2fce6918a17efc
Lembree, Dennis <weboverhauls@gmail.com>
2013-08-15 17:33:05 -0700

    Adjusting layout of "select page" bullets on Retweets pages.

--- site/application/views/retweets.php
+++ site/application/views/retweets.php
@@ -1,11 +1,11 @@
 <h1 class="rounded"><?php echo $xliff_reader->get('nav-retweets'); ?></h1>
 
-<div class="box1 rounded">
+<div class="box1 rounded" style="margin-bottom: -.5rem;">
 	<h2><?php echo $xliff_reader->get('gbl-select-page'); ?></h2>
-	<ul>
-		<li><a href="/retweets/by_me"><?php echo $xliff_reader->get('nav-retweets-by-me'); if ($type=="by_me") echo " (current)"; ?></a></li>
-		<li><a href="/retweets/of_me"><?php echo $xliff_reader->get('nav-retweets-of-me'); if ($type=="of_me") echo " (current)"; ?></a></li>
-		<li><a href="/retweets/to_me"><?php echo $xliff_reader->get('nav-retweets-to-me'); if ($type=="to_me") echo " (current)"; ?></a></li>
+	<ul class="clearfix">
+		<li class="fl"><a href="/retweets/by_me"><?php echo $xliff_reader->get('nav-retweets-by-me'); if ($type=="by_me") echo " (current)"; ?></a></li>
+		<li class="fl"><a href="/retweets/of_me"><?php echo $xliff_reader->get('nav-retweets-of-me'); if ($type=="of_me") echo " (current)"; ?></a></li>
+		<li class="fl"><a href="/retweets/to_me"><?php echo $xliff_reader->get('nav-retweets-to-me'); if ($type=="to_me") echo " (current)"; ?></a></li>
 	</ul>
 </div>
 
