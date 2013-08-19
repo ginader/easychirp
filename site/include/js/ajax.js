ed9fe0a997459aac66cc86f1b8430b9cd619a030
weboverhauls <weboverhauls@gmail.com>
2013-08-18 17:03:02 -0700

    Moving 4 vars into 1 object for JS optimization.

--- site/include/js/ajax.js
+++ site/include/js/ajax.js
@@ -6,23 +6,24 @@ $('a[href*="favorite_"]').click(function(e) {
 	var a = $(this);
 	var url_replace = this.href;
 	var url_send = this.href.replace("false","true");
-	var txtMakeFav      = $("#main").attr("data-fav-make"); //"make favorite";
-	var txtRemoveFav    = $("#main").attr("data-fav-remove"); //"remove favorite";
-	var txtAlertAdded   = $("#main").attr("data-fav-alert-added"); //"The favorite has been added.";
-	var txtAlertRemoved = $("#main").attr("data-fav-alert-removed"); //"The favorite has been removed.";	
+	var txt = {};
+	txt.MakeFav      = $("#main").attr("data-fav-make"); //"make favorite";
+	txt.RemoveFav    = $("#main").attr("data-fav-remove"); //"remove favorite";
+	txt.AlertAdded   = $("#main").attr("data-fav-alert-added"); //"The favorite has been added.";
+	txt.AlertRemoved = $("#main").attr("data-fav-alert-removed"); //"The favorite has been removed.";	
 
 	if (url_replace.indexOf("create") != -1) {
 		$.ajax({
 			url: url_send,
 			success: function(response) {
-				alert(txtAlertAdded);
+				alert(txt.AlertAdded);
 				
 				$(a).addClass("favorited");
 
 				url_replace = url_replace.replace(/create/,"destroy");
 				$(a).attr("href", url_replace);
 
-				$(a).attr("title", txtRemoveFav);
+				$(a).attr("title", txt.RemoveFav);
 			},
 			error: function(xhr) {
 				alert('Error. Status = ' + xhr.status);
@@ -33,14 +34,14 @@ $('a[href*="favorite_"]').click(function(e) {
 		$.ajax({
 			url: url_send,
 			success: function(response) {
-				alert(txtAlertRemoved);
+				alert(txt.AlertRemoved);
 				
 				$(a).removeClass("favorited");
 
 				url_replace = url_replace.replace(/destroy/,"create");
 				$(a).attr("href", url_replace);
 
-				$(a).attr("title", txtMakeFav);
+				$(a).attr("title", txt.MakeFav);
 			},
 			error: function(xhr) {
 				alert('Error. Status = ' + xhr.status);
