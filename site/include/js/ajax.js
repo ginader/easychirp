cbacad1484cb7113428d42af3c6addef90bfabab
Lembree, Dennis <weboverhauls@gmail.com>
2013-08-23 21:27:20 -0700

    Functionality for Retweet (AJAX) - Issue #108

--- site/include/js/ajax.js
+++ site/include/js/ajax.js
@@ -131,3 +131,59 @@ $('a[href*="direct_delete"]').click(function(e) {
 	})
 });
 
+
+// Ajax for creating a retweet // will do destroying later
+$('a[href*="retweet_"]').click(function(e) {
+	e.preventDefault();
+
+	var a = $(this);
+	var url_replace = this.href;
+	var url_send = this.href.replace("false","true");
+	var txt = {};
+	txt.MakeRt       = $("#main").attr("data-rt-make"); //"";
+	txt.RemoveRt     = $("#main").attr("data-rt-remove"); //"";
+	txt.AlertAdded   = $("#main").attr("data-rt-alert-added"); //"";
+	txt.AlertRemoved = $("#main").attr("data-rt-alert-removed"); //"";
+
+	//alert(txt.MakeRt + " " + txt.RemoveRt + " " + txt.AlertAdded + " " + txt.AlertRemoved);
+	//return false;
+
+	if (url_replace.indexOf("create") != -1) {
+		$.ajax({
+			url: url_send,
+			success: function(response) {
+				alert(txt.AlertAdded);
+				
+				$(a).addClass("retweeted");
+
+				//url_replace = url_replace.replace(/create/,"destroy");
+				//$(a).attr("href", url_replace);
+				$(a).removeAttr("href");
+
+				//$(a).attr("title", txt.RemoveRt);
+				$(a).attr("title", "retweeted");
+			},
+			error: function(xhr) {
+				alert('Error. Status = ' + xhr.status);
+			}
+		})
+	}
+	// else {
+	// 	$.ajax({
+	// 		url: url_send,
+	// 		success: function(response) {
+	// 			alert(txt.AlertRemoved);
+				
+	// 			$(a).removeClass("favorited");
+
+	// 			url_replace = url_replace.replace(/destroy/,"create");
+	// 			$(a).attr("href", url_replace);
+
+	// 			$(a).attr("title", txt.MakeFav);
+	// 		},
+	// 		error: function(xhr) {
+	// 			alert('Error. Status = ' + xhr.status);
+	// 		}
+	// 	})
+	// }
+});
