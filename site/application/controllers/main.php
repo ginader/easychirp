c7cebbe12340c83f564e7e05afcf29566adbe341
weboverhauls <weboverhauls@gmail.com>
2013-08-24 13:40:47 -0700

    Changing tweets displayed on Profile page from 10 to 1

--- site/application/controllers/main.php
+++ site/application/controllers/main.php
@@ -994,12 +994,12 @@ class Main extends EC_Controller {
 		$this->twitter_lib->connect($params);
 
 		$request_param = array();	
-		$request_param['screen_name'] =  $this->session->userdata('screen_name');
+		$request_param['screen_name'] = $this->session->userdata('screen_name');
 
 		$profile = $this->twitter_lib->get('users/show', $request_param );
 		$this->_data['profile'] = $profile;
 
-		$request_param['count'] = 10; // This doesn't use TWEETS_PER_PAGE because it should only show a subset
+		$request_param['count'] = 1; // This doesn't use TWEETS_PER_PAGE because it should only show a subset
 		$tweets = $this->twitter_lib->get('statuses/user_timeline', $request_param );
 
 		$this->_data['tweets'] = $this->load->view('fragments/tweet', 
