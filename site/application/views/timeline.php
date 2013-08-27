669b46f178f66f938dba4d3bcc2098e96d4739ba
Lembree, Dennis <weboverhauls@gmail.com>
2013-08-26 19:57:22 -0700

    For delete tweet, adding confirmation message and translations. Issue #117

--- site/application/views/timeline.php
+++ site/application/views/timeline.php
@@ -1,7 +1,14 @@
+<?php
+if (isset($_GET["action"])) {
+	if ($_GET["action"] == "tweet_deleted") {
+		echo '<div class="msgBoxPos rounded">'.$xliff_reader->get('msg-tweet-deleted').'</div>';
+	}
+}
+?>
+
 <h1 class="rounded"><?php echo $page_heading; ?></h1>
 
 <?php
 echo $write_tweet_form;
 echo $tweets;
-
 
