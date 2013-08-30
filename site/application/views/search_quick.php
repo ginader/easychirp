9f7e6b481011a827f184fce2893e827a3af379bd
Andrew Woods <atwoods1@gmail.com>
2013-08-29 21:10:52 -0700

    Bug Fix #91 - removed obsolete files

--- site/application/views/search_quick.php
+++
@@ -1,7 +0,0 @@
-<h1 class="rounded"><?php echo $xliff_reader->get('nav-quick-search'); ?></h1>
-
-<div class="box1 rounded" style="padding: 1.5rem;">
-
-<?php require_once 'fragments/search_quick_form.php'; ?>
-
-</div>
