6e2746decb372656b2ac9138765a2bfff9568b02
Lembree, Dennis <weboverhauls@gmail.com>
2013-09-03 10:27:22 -0700

    fixing user timeline link on User page

--- site/application/views/user.php
+++ site/application/views/user.php
@@ -125,7 +125,7 @@ if (isset($_GET["action"])) {
 		</dd>
 
 		<dt><?php echo $xliff_reader->get('profile-dt-tweets'); ?></dt>
-		<dd><a href="user_timeline?user=<?php echo $user->screen_name; ?>"><?php echo $user->statuses_count; ?></a></dd>
+		<dd><a href="/user_timeline/<?php echo $user->screen_name; ?>"><?php echo $user->statuses_count; ?></a></dd>
 
 		<dt><?php echo $xliff_reader->get('profile-dt-following'); ?></dt>
 		<dd><a href="/following?id=<?php echo $user->screen_name; ?>"><?php echo $user->friends_count; ?></a></dd>
