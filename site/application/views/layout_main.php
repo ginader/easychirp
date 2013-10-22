25eef39cdbf2e61e856f607710b16e653897681f
Lembree, Dennis <weboverhauls@gmail.com>
2013-10-22 12:16:20 -0700

    adding "beta" to logo and making transparent

--- site/application/views/layout_main.php
+++ site/application/views/layout_main.php
@@ -28,7 +28,7 @@
 <?php endif; ?> <a href="#main"><?php echo $xliff_reader->get('skip-main-content'); ?></a></div>
 
 <header role="banner">
-	<div id="logoContainer"><img src="/images/brand/EasyChirp_Logo2_300.png" alt="Easy Chirp 2" width="300" height="109" /></div>
+	<div id="logoContainer"><img src="/images/brand/EasyChirp_Logo2_300_beta.png" alt="Easy Chirp 2 beta" width="300" height="109" /></div>
 	<div id="welcome">
 		<?php if ($logged_in): ?>
 			<h2 class="hide">My Info</h2>
