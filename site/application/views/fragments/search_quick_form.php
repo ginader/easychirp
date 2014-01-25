8884313ab8b042b46e7f4f8fe2b5726fa839c3f7
Lembree, Dennis <weboverhauls@gmail.com>
2014-01-24 21:31:39 -0800

    Adding x-webkit-speech attribute to search inputs (for speech-to-text in Chrome!)

--- site/application/views/fragments/search_quick_form.php
+++ site/application/views/fragments/search_quick_form.php
@@ -9,7 +9,7 @@
 ?>
 <form id="frmSearch" action="/search_results" method="post">
 	<label for="query" class="hide"><?php echo $xliff_reader->get('search-tweets-query'); ?></label>
-	<input name="query" id="query" type="text" size="35" maxlength="50" class="input1" required aria-required="true" <?php
+	<input x-webkit-speech name="query" id="query" type="text" size="35" maxlength="50" class="input1" required aria-required="true" <?php
 		if (isset($meta->query)) {
 			echo 'value="' . urldecode($meta->query) . '"';
 		}
