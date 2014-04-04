765e0f4c064b481c9e7f69501c32e3f4e3435584
Lembree, Dennis <dlembree@paypal.com>
2014-04-04 15:57:46 -0700

    Changing Prod from .org to .com

--- site/application/config/production/config.php
+++ site/application/config/production/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url'] = 'http://www.easychirp.org/';
+$config['base_url'] = 'http://www.easychirp.com/';
 
 /*
 |--------------------------------------------------------------------------
