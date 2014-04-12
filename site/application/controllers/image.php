70b84e3388a3c34ce0ad10ce1161d9b2fd02c5b2
Lembree, Dennis <dlembree@paypal.com>
2014-04-12 11:03:22 -0700

    Beginning pieces to: Add custom image page view #152

---
+++ site/application/controllers/image.php
@@ -0,0 +1,45 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+/**
+ * Image Controller for EasyChirp
+ *
+ * @package EasyChirp
+ * @subpackage Controllers
+ * @author EasyChirp Team
+ */
+class Image extends CI_Controller {
+
+	public $_data = array();
+
+	/**
+	 * Manages the image page - /img
+	 * this page is public
+	 */
+	public function img($id)
+	{
+		// Use $id to get image data from service
+		// Parse the image URL, title, and description
+		// Use the 'image' template to output the HTML
+
+		// Dummy/fake data (would come from API)
+		$url = "http://www.webaxe.org/wp-content/uploads/2013/08/tl-horizontal_main1.jpg";
+		$title = "Coffee Cup Caffeine Infographic";
+		$longdesc = "<p>9 kinds of coffee, each with a coffee mug that displays an amount of coffee relative to the level of caffeine. Each amount below is in milligrams of caffeine per fluid ounce.</p><ol reversed><li>McDonald&rsquo;s: 9.1</li><li>Seattle&rsquo;s Best: 10.4</li><li>Biggby Coffee: 12.5</li><li>Dunkin&rsquo;Donuts: 12.7</li><li>Dutch Bros. Coffee: 12.8</li><li>Caribou Coffee: 15</li><li>Peet&rsquo;s: 16.7</li><li>Starbucks: 20.6</li><li>Deathwish coffee: 54.2</li></ol>";
+		
+		// Set page data vars
+		$_data["url"] = $url;
+		$_data["title"] = $title . " " . $id;
+		$_data["longdesc"] = $longdesc;
+		
+		// Create dataURI for longdesc page
+		$longdescStr = '<html><head><title>Description: '.$title.'</title><meta charset="utf-8"></head><body>'.$longdesc.'</body></html>';
+		$_data["longdescUri"] = "data:text/html;base64," . base64_encode($longdescStr);
+
+		$this->load->view("img", $_data);
+	}
+
+}
+
+
+/* End of file image.php */
+/* Location: ./application/controllers/image.php */
---
+++ site/application/views/img.php
@@ -0,0 +1,60 @@
+<!doctype html>
+<html lang="en">
+<head>
+     <meta charset="utf-8" />
+     <title>View Image: <?php echo $title; ?></title>
+     
+     <meta name="description" content="Image view including long description; provided by Easy Chirp." />
+     <meta name="author" content="Easy Chirp | http://www.EasyChirp.com | @EasyChirp" />
+     <meta name="viewport" content="width=device-width, initial-scale=1">
+     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+
+     <link rel="shortcut icon" href="/images/brand/favicon.ico"/>
+     <link rel="stylesheet" href="/include/css/general.css" />
+     <style type="text/css">
+     h1 {
+          margin-bottom: .5em;
+     }
+     img {
+          margin-left: .75em;
+          max-width: 1000px;
+          height: auto;
+     }
+     footer p {
+          margin-left: .75em;
+          margin-bottom: .25em;
+          border-top: 3px solid #ccc;
+     }
+     /*footer p,
+     footer div {
+          text-align: right;
+     }*/
+     </style>
+</head>
+
+<body>
+
+<div id="wrapper">
+
+     <header role="banner">
+          <h1 class="rounded">View Image: <?php echo $title; ?></h1>
+     </header>
+
+     <main role="main">
+          <div><img src="<?php echo $url; ?>" alt="<?php echo $title; ?>" longdesc="<?php echo $longdescUri; ?>" /></div>
+          <!-- <div>
+               <p><?php echo $title; ?></p>
+               <p><?php echo $longdesc; ?></p>
+               <a href="<?php echo $longdescUri; ?>">KDLKF</a>
+          </div> -->
+     </main>
+
+     <footer role="contentinfo">
+          <p>Brought to you by <a href="http://www.easychirp.com">Easy Chirp</a> | <a href="http://twitter.com/easychirp">@EasyChirp</a></p>
+          <div><img src="/images/brand/easy_chirp_icon1.png" width="48" height="48" alt="logo" /></div>
+     </footer>
+
+</div>
+
+</body>
+</html>
