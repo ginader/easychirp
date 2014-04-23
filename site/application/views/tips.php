ab0bd2c85885b3dd7e540bb59640f3ba1a40d4a7
Lembree, Dennis <dlembree@paypal.com>
2014-04-23 10:28:39 -0700

    updating a couple app links

--- site/application/views/tips.php
+++ site/application/views/tips.php
@@ -85,9 +85,9 @@
 	<h2><?php echo $xliff_reader->get('tips-h2-apps'); ?></h2>
 	<ul>
 		<li><a href="http://twuffer.com/" rel="external">Twuffer</a>: a Twitter buffer (schedule tweets)</li>
-		<li><a href="http://topsy.com/" rel="external">Topsy</a>: Twitter search.</li>
+		<li><a href="http://topsy.com/tweets" rel="external">Topsy</a>: Twitter search.</li>
 		<li><a href="http://backtweets.com/" rel="external">BackTweet</a>: Search links on Twitter.</li>
-		<li><a href="http://monitter.com/" rel="external">Monitter</a>: monitor tweets with keywords.</li>
+		<li><a href="http://www.tweetwally.com/" rel="external">TweetWally</a>: create custom streams.</li>
 		<li><a href="http://tweetstats.com" rel="external">TweetStats</a>: Graphin' your stats!</li>
 		<li><a href="http://twtvite.com/" rel="external">Twtvite</a>: Organize a tweetup!</li>
 		<li><a href="http://twtpoll.com/" rel="external">TwtPoll</a>: create and distribute polls on Twitter.</li>
