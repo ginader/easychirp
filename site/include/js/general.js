949a80aeb47dfc71d813646075ea7482bf2de4eb
weboverhauls <weboverhauls@gmail.com>
2014-06-07 16:09:08 -0700

    Adding keyboad shortcut for quick search; making that and the search shortcut onkeyup rather than keydown.

--- site/include/js/general.js
+++ site/include/js/general.js
@@ -212,7 +212,8 @@ function resizeMask() {
     $('#mask').css('height', maskHeight);
 }
 
-$('a[rel=modal]').click(function(e) {
+// Handle links to modals
+$('a[rel=modal]').on('click', function(e) {
 	e.preventDefault();
 	var id = $(this).attr('href').replace("/","#");
 	modalOpen = true;
@@ -418,18 +419,18 @@ $("#tweet_menu_wrapper").accessibleMegaMenu({
  * jQuery Hotkeys Plugin
  * Copyright 2010, John Resig
  * Dual licensed under the MIT or GPL Version 2 licenses.
- *
- * Based upon the plugin by Tzury Bar Yochay:
- * http://github.com/tzuryby/hotkeys
- *
- * Original idea by:
- * Binny V A, http://www.openjs.com/scripts/events/keyboard_shortcuts/
+ * https://github.com/jeresig/jquery.hotkeys
  */
 (function(e){function t(t){if(typeof t.data==="string"){t.data={keys:t.data}}if(!t.data||!t.data.keys||typeof t.data.keys!=="string"){return}var n=t.handler,r=t.data.keys.toLowerCase().split(" "),i=["text","password","number","email","url","range","date","month","week","time","datetime","datetime-local","search","color","tel"];t.handler=function(t){if(this!==t.target&&(/textarea|select/i.test(t.target.nodeName)||e.inArray(t.target.type,i)>-1)){return}var s=e.hotkeys.specialKeys[t.keyCode],o=String.fromCharCode(t.which).toLowerCase(),u="",a={};e.each(["alt","ctrl","meta","shift"],function(e,n){if(t[n+"Key"]&&s!==n){u+=n+"+"}});u=u.replace("alt+ctrl+meta+shift","hyper");if(s){a[u+s]=true}if(o){a[u+o]=true;a[u+e.hotkeys.shiftNums[o]]=true;if(u==="shift+"){a[e.hotkeys.shiftNums[o]]=true}}for(var f=0,l=r.length;f<l;f++){if(a[r[f]]){return n.apply(this,arguments)}}}}e.hotkeys={version:"0.8",specialKeys:{8:"backspace",9:"tab",10:"return",13:"return",16:"shift",17:"ctrl",18:"alt",19:"pause",20:"capslock",27:"esc",32:"space",33:"pageup",34:"pagedown",35:"end",36:"home",37:"left",38:"up",39:"right",40:"down",45:"insert",46:"del",59:";",61:"=",96:"0",97:"1",98:"2",99:"3",100:"4",101:"5",102:"6",103:"7",104:"8",105:"9",106:"*",107:"+",109:"-",110:".",111:"/",112:"f1",113:"f2",114:"f3",115:"f4",116:"f5",117:"f6",118:"f7",119:"f8",120:"f9",121:"f10",122:"f11",123:"f12",144:"numlock",145:"scroll",173:"-",186:";",187:"=",188:",",189:"-",190:".",191:"/",192:"`",219:"[",220:"\\",221:"]",222:"'"},shiftNums:{"`":"~",1:"!",2:"@",3:"#",4:"$",5:"%",6:"^",7:"&",8:"*",9:"(",0:")","-":"_","=":"+",";":": ","'":'"',",":"<",".":">","/":"?","\\":"|"}};e.each(["keydown","keyup","keypress"],function(){e.event.special[this]={add:t}})})(this.jQuery)
 
-/* Keyboard shortcut; pressing "/" goes to Search page */
-$(document).bind('keydown', '/', function() {
+// pressing "/" goes to Search page
+$(document).bind('keyup', '/', function() {
 	window.location.href = "/search";
+});
+
+// pressing ctrl+/ goes to Quick Search modal
+$(document).bind('keyup', 'ctrl+/', function() {
+	$("a[href='/search_quick']").trigger("click");
 });
 
 
