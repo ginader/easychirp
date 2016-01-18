6199c3aff6103ba123c9c8fcbdcb8a1bda8d9e32
weboverhauls <weboverhauls@gmail.com>
2016-01-18 18:43:55 -0500

    Fixing bug with hashtag links that include a special character.

--- site/application/views/fragments/tweet.php
+++ site/application/views/fragments/tweet.php
@@ -73,7 +73,7 @@ foreach($tweets AS $tweet):
 	$tweet_text = preg_replace('/@+([-_0-9a-zA-Z]+)/', '<a href="/user/$1">$0</a>', $tweet_text);
 
 	// Link #hashtags
-	$tweet_text = preg_replace('/\B#([-_0-9a-zA-Z]+)/', '<a href="/search_results?query=%23$1">$0</a>', $tweet_text);
+	$tweet_text = preg_replace('/\B#([-_0-9a-zA-ZàèìòùÀÈÌÒÙáéíóúýÁÉÍÓÚÝâêîôûÂÊÎÔÛãñõÃÑÕäëïöüÿÄËÏÖÜŸçÇßØøÅåÆæœ]+)/', '<a href="/search_results?query=%23$1">$0</a>', $tweet_text);
 
 	// Output tweet text;
 	echo $tweet_text;
