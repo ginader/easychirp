91c54599f761999894413c05c4f7f73df2ff0dec
Dennis Lembree <dennislembree@Denniss-MacBook-Pro.local>
2017-03-04 12:05:48 -0800

    Translations—mostly for #198

--- site/application/views/features.php
+++ site/application/views/features.php
@@ -6,8 +6,8 @@
 <div class="box1 rounded">
 	<h2><?php echo $xliff_reader->get('features-a11y-h2'); ?></h2>
 	<ul>
-		<li><?php echo $xliff_reader->get('home-features-img'); ?></li>
-		<li>Alternative text for an image is rendered for screen reader users and also can be displayed for sighted users (when alternative text is provided by the Twitter API).</li>
+		<li><?php echo $xliff_reader->get('home-features-img'); ?>
+		<li><?php echo $xliff_reader->get('features-a11y-alt'); ?>
 		<li><?php echo $xliff_reader->get('features-a11y-kybd'); ?>
 		<li><?php echo $xliff_reader->get('features-a11y-layout'); ?>
 		<li><?php echo $xliff_reader->get('features-a11y-headings'); ?>
@@ -30,7 +30,7 @@
 <div class="box1 rounded">
 	<h2><?php echo $xliff_reader->get('features-main-h2'); ?></h2>
 	<ul>
-		<li>Political filter removes tweets with "Trump" and a few other related terms. Toggle from footer.</li>
+		<li><?php echo $xliff_reader->get('features-main-pol'); ?>
 		<li><?php echo $xliff_reader->get('features-main-shortener'); ?>
 		<li><?php echo $xliff_reader->get('features-main-replytoall'); ?>
 		<li><?php echo $xliff_reader->get('features-main-lists'); ?>
