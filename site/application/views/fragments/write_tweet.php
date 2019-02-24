23c582b6e3bc8a85ce596c562400de52e259b5c2
Dennis Lembree <>
2019-02-24 16:38:11 -0500

    Programmatically associating the tweet hint (char remaining) with the tweet input

--- site/application/views/fragments/write_tweet.php
+++ site/application/views/fragments/write_tweet.php
@@ -28,7 +28,7 @@ if (empty($single)):
 			data-error-over="<?php echo $xliff_reader->get('error-over-limit'); ?>"
 			data-error-empty="<?php echo $xliff_reader->get('error-tweet-empty'); ?>">
 			<div>
-				<textarea id="txtEnterTweet" name="status" rows="3"><?php
+				<textarea id="txtEnterTweet" name="status" rows="3" aria-describedby="charlimit"><?php
 
 				if ( isset($reply_to) )
 				{
